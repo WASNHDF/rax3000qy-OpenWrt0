7f0265dceaa8db3c965b159809405048391cdd98
WASNHDF <52650540@qq.com>
2026-06-24 20:00:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# 仅拉取Passwall源码
-git clone https://mirror.ghproxy.com/https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone https://mirror.ghproxy.com/https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
